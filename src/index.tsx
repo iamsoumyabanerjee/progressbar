702285986141f3828bc62c1eb49b8e0851cc15c6
iamsoumyabanerjee <06.banerjee@gmail.com>
2023-02-26 17:24:08 +1100

    formated with prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 export interface IAppProps {
-  bar: number[];
-  controllers: number[];
+    bar: number[];
+    controllers: number[];
 }
 
 const config: IAppProps = {
-  bar: [25, 50, 75],
-  controllers: [-25, -10, 10, 25],
+    bar: [25, 50, 75],
+    controllers: [-25, -10, 10, 25]
 };
 
 root.render(
-  <React.StrictMode>
-    <App config={config} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App config={config} />
+    </React.StrictMode>
 );
